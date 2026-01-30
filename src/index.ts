ce251dfd5f0820239d737edd696c3bca7dda346a
omaresam0 <omaresam2000@hotmail.com>
2026-01-30 02:14:30 +0200

    Product feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoute from "./routes/userRoute.js"
+import productRoute from "./routes/productRoute.js"
+import {seedInitialProducts} from "./services/productService.js"
 
 const app = express()
 const port = 3001;
@@ -8,7 +10,11 @@ app.use(express.json());
 mongoose.connect("mongodb://localhost:27017/ecommerce").then( () => console.log("Mongo connected!"))
 .catch((err) => console.log("Failed to connect!", err));
 
+// seed the initial products
+seedInitialProducts();
+
 app.use('/user', userRoute);
+app.use('/product', productRoute);
 
 app.listen(port, () => {
     console.log(`Serveris running at ${port}`);
---
+++ src/services/productService.ts
@@ -0,0 +1,26 @@
+import productModel from "../models/productModel.js";
+
+export const getAllProducts = async () => {
+    return await productModel.find();
+}
+
+export const seedInitialProducts = async () => {
+    const products = [
+        { title: "Product 1", image: "https://m.media-amazon.com/images/I/41Dg9haNatL.jpg", price: 10, stock: 100 },
+        { title: "Product 2", image: "image2.jpg", price: 13, stock: 11 },
+        { title: "Product 3", image: "image3.jpg", price: 30, stock: 33 },
+        { title: "Product 4", image: "image4.jpg", price: 40, stock: 2 },
+        { title: "Product 5", image: "image5.jpg", price: 17, stock: 54 },
+        { title: "Product 6", image: "image6.jpg", price: 13, stock: 32 },
+        { title: "Product 7", image: "image7.jpg", price: 12, stock: 12 },
+        { title: "Product 8", image: "image8.jpg", price: 9, stock: 1 },
+        { title: "Product 9", image: "image9.jpg", price: 34, stock: 11 },
+        { title: "Product 10", image: "image10.jpg", price: 22, stock: 33 },
+    ];
+
+    const existsingProducts = await getAllProducts();
+
+    if(existsingProducts.length == 0){
+        await productModel.insertMany(products);
+    }
+}
